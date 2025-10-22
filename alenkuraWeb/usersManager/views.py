913973ad6b55e9153788275b54eec6686f390efa
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-22 13:14:09 -0300

    CToph02: agregan funciones para que funcionen las urls

--- alenkuraWeb/usersManager/views.py
+++ alenkuraWeb/usersManager/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from services.auth.login_service import Log_in, Log_out
 
 # Create your views here.
-def hola(request):
-    return HttpResponse("Hola")
+def login(request):
+    return render(request, "login.html", {})
+
+def login(request):
+    return Log_in(request)
+
+def logout(request):
+    return Log_out(request)
+
+def index(request):
+    return render(request, "index.html", {})
