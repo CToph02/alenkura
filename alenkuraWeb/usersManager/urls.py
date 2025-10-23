1b8591de21dc05bb8ad797c25d8364eb48056fe2
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-22 23:54:57 -0300

    CToph02: se agregaron url director y docente

--- alenkuraWeb/usersManager/urls.py
+++ alenkuraWeb/usersManager/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('index', views.index, name='index'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('director_dashboard', views.director_dashboard, name='director_dashboard'),
+    path('docent_dashboard', views.docent_dashboard, name='docent_dashboard')
 ]
