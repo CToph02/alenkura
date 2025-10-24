4727b4192deb869a0fef8d9a53785bb7a83e3788
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-24 16:03:22 -0300

    CToph02: se modificó urls de userManager agregando función create_docent

--- alenkuraWeb/usersManager/urls.py
+++ alenkuraWeb/usersManager/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('index', views.index, name='index'),
     path('logout', views.logout, name='logout'),
-    path('director_dashboard', views.director_dashboard, name='director_dashboard'),
-    path('docent_dashboard', views.docent_dashboard, name='docent_dashboard')
+    path('index_director', views.index_director, name='index_director'),
+    path('add_docent', views.addDocent, name='add_docent'),
+    path('create_docent', views.create_docent, name='create_docent')
 ]
