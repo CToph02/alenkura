4fbdb6dbc4c1eacbad601033a4b4cefbaac51455
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-24 16:03:54 -0300

    CToph02: se modificó models de userManager agregando más campos a User

--- alenkuraWeb/usersManager/models.py
+++ alenkuraWeb/usersManager/models.py
@@ -11,8 +11,8 @@ class User(models.Model):
     email = models.CharField(max_length=150)
     active = models.BooleanField()
     fk_rol = models.ForeignKey("Rol", on_delete=models.CASCADE)
-    fk_category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    fk_category = models.ForeignKey("UserCategory", on_delete=models.CASCADE)
 
-class Category(models.Model):
+class UserCategory(models.Model):
     category_id = models.AutoField(primary_key=True)
     name_category = models.CharField(max_length=20)
