12c253c6a1953301e34fc382b24d7ceff31c468e
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-25 18:18:56 -0300

    CToph02: se agregaron views para las urls paci, cursos, notas, listaestudiantes, etc.

--- alenkuraWeb/usersManager/views.py
+++ alenkuraWeb/usersManager/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.models import Group, User
+from django.shortcuts import render
 from .services.auth_service import log_in, log_out
-from .services.add_docent import create_docent
+from .services.add_docent import add_docent
+from .services.add_students import add_student
 
 # Create your views here.
 def login(request):
@@ -11,11 +12,39 @@ def logout(request):
     return log_out(request)
 
 def addDocent(request):
-    return render(request, "userManager/add_docent.html")
+    grupos = Group.objects.all()
+    context = {
+        'grupos': grupos
+    }
+    return render(request, "userManager/add_docent.html", context)
+
+def addStudent(request):
+    return render(request, "userManager/add_student.html")
 
 def create_docent(request):
-    return create_docent(request)
+    return add_docent(request)
 
+def create_student(request):
+    return add_student(request)
 
-def index_director(request):
-    return render(request, "index_director.html")
+def index_administracion(request):
+    return render(request, "index_administracion.html")
+
+def index_docent(request):
+    return render(request, "index_docent.html")
+
+def paci(request):
+    return render(request, "paci.html")
+
+def cursos(request):
+    return render(request, "cursos.html")
+
+def notas(request):
+    return render(request, "notas.html")
+
+def listaestudiantes(request):
+    usuarios = User.objects.all()
+    context = {
+        "usuarios": usuarios
+    }
+    return render(request, "listaestudiantes.html", context)
