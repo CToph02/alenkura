cbfd8e776b5834b433a9f7f0ede5fd8fdd3d8890
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-25 18:17:31 -0300

    CToph02: logica para administrar docentes

--- alenkuraWeb/usersManager/services/add_docent.py
+++ alenkuraWeb/usersManager/services/add_docent.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 
-def create_docent(request):
+def add_docent(request):
     username = request.POST.get('username')
     email = request.POST.get('email')
     phone = request.POST.get('phone')
+    type = request.POST.get('typeDocent')
 
-    print(username, email, phone)
+    print(username, email, phone, type)
     return redirect('add_docent')
+
+def delete_docent(request):
+    pass
+
+def update_docent(request, id):
+    pass
+
+def delete_docent(request, id):
+    pass
