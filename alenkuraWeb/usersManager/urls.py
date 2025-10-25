0215ec40be33c01d7cdd74a565fced8680007cd2
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-25 18:18:32 -0300

    CToph02: se agregaron urls para paci, cursos, notas, listaestudiantes, etc.

--- alenkuraWeb/usersManager/urls.py
+++ alenkuraWeb/usersManager/urls.py
@@ -4,7 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('index_director', views.index_director, name='index_director'),
+    path('index_administracion', views.index_administracion, name='index_administracion'),
+    path('index_docent', views.index_docent, name='index_docent'),
     path('add_docent', views.addDocent, name='add_docent'),
-    path('create_docent', views.create_docent, name='create_docent')
+    path('add_student', views.addStudent, name='add_student'),
+    path('create_docent', views.create_docent, name='create_docent'),
+    path('create_student', views.create_student, name='create_student'),
+    path('paci', views.paci, name='paci'),
+    path('cursos', views.cursos, name='cursos'),
+    path('notas', views.notas, name='notas'),
+    path('listaestudiantes', views.listaestudiantes, name='listaestudiantes')
 ]
