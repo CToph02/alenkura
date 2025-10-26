4cf8fb8803470a38c14e67573a78747630192fb5
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-26 00:06:38 -0300

    CToph02: se registró tabla rol en el admin

--- alenkuraWeb/coreBD/admin.py
+++ alenkuraWeb/coreBD/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Asignaturas
+from .models import Asignaturas, Rol
 # Register your models here.
 admin.site.register(Asignaturas)
+admin.site.register(Rol)
