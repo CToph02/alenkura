b3dbbab0e781bb1a175645b95456c74cb1182c10
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-26 00:06:13 -0300

    CToph02: se agregó tabla rol en coreBD y registrado en el admin

--- alenkuraWeb/coreBD/models.py
+++ alenkuraWeb/coreBD/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class DateTime(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True, null=True)
+    updatedAt = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        abstract = True
+
 class Asignaturas(models.Model):
     nombre_asignatura = models.CharField(max_length=50)
+
+class Rol(models.Model):
+    rol_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=15)
