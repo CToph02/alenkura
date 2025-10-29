4cfdc391b89fff3bc571e01a3d896b71bd54b2ed
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-29 19:10:58 -0300

    CToph02: se registraron las tablas Rol, Curso y Nivel

--- alenkuraWeb/coreBD/admin.py
+++ alenkuraWeb/coreBD/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Asignaturas
+from .models import Asignatura, Rol, Curso, Nivel
 # Register your models here.
-admin.site.register(Asignaturas)
+
+admin.site.register(Asignatura)
+admin.site.register(Rol)
+admin.site.register(Curso)
+admin.site.register(Nivel)
