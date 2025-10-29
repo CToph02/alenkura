697fc0ef33056cf14574bee5e799e4a9d1315711
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-29 19:11:47 -0300

    CToph02: se agregaron las tablas Rol. Curso y Nivel

--- alenkuraWeb/coreBD/models.py
+++ alenkuraWeb/coreBD/models.py
@@ -2,12 +2,33 @@ from django.db import models
 
 # Create your models here.
 class DateTime(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True, null=True)
-    updatedAt = models.DateTimeField(auto_now_add=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True, editable=True)
+    updatedAt = models.DateTimeField(auto_now=True, editable=True)
 
     class Meta:
         abstract = True
 
-class Asignaturas(models.Model):
-    nombre_asignatura = models.CharField(max_length=50)
+class Asignatura(models.Model):
+    asignatura = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.asignatura}"
+
+class Rol(models.Model):
+    rol = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return f"{self.rol}"
+    
+class Nivel(models.Model):
+    nivel = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return f"{self.nivel}"
+    
+class Curso(models.Model):
+    curso = models.CharField(max_length=30)
+    fk_nivel = models.ForeignKey(Nivel, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.curso} - {self.fk_nivel}"
