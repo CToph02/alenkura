c707b454df2c17c88c8c161a0787e1c7a6d79e1e
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-29 19:20:12 -0300

    CToph02: se quitó función register_docent

--- alenkuraWeb/usersManager/services/auth_service.py
+++ alenkuraWeb/usersManager/services/auth_service.py
@@ -4,16 +4,21 @@ from django.contrib.auth import authenticate, login, logout
 def log_in(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
+    print(username)
+    print(password)
 
-    user = authenticate(username=username, password=password)
+    user = authenticate(name=username, password=password)
+
+    print(user)
 
     if user is not None:
         login(request, user)
-        
+        print(user.rol)
+
         if user.is_superuser:
             return redirect('index_administracion')
         
-        elif user.is_staff:
+        elif user.is_docent:
             return redirect('index_docent')
             
     return render(request, 'login.html')
@@ -21,6 +26,3 @@ def log_in(request):
 def log_out(request):
     logout(request)
     return redirect('login')
-
-def register_docent(request):
-    pass
