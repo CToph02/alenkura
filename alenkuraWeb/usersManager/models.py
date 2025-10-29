3c57bdefd47185cc2981c74ca6589615b4c30510
CToph02 <christopher.munoz.0220@gmail.com>
2025-10-29 19:15:17 -0300

    CToph: Se agregó tabla UserManager con metodos para crear los usuarios y se modificó tabla User para hacer uso de la tabla UserManager

--- alenkuraWeb/usersManager/models.py
+++ alenkuraWeb/usersManager/models.py
@@ -1,27 +1,71 @@
 from django.db import models
 from coreBD.models import DateTime
-
-class Rol(models.Model):
-    rol_id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=15)
-
-class UserCategory(DateTime):
-    category_id = models.AutoField(primary_key=True)
-    name_category = models.CharField(max_length=20)
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from coreBD.models import Nivel, Curso
     
+class UserManager(BaseUserManager):
+    def create_user(self, name, rol, email, password=None, **extra_fields):
+        from coreBD.models import Rol
+
+        if not email:
+            raise ValueError("El usuario debe tener un correo electrónico")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            rol=rol,
+            **extra_fields
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, name, email, password=None, **extra_fields):
+        from coreBD.models import Rol
+
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        user = self.create_user(
+            email=self.normalize_email(email), 
+            name=name,
+            password=password,
+            **extra_fields
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin, DateTime):
+    from coreBD.models import Rol
+    name = models.CharField(max_length=100, unique=True)
+    email = models.CharField(verbose_name="email address",max_length=150, null=True)
+
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, null=True)
+    
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_docent = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = ['email']
+
     def __str__(self):
-        return f"{self.name_category}"
+        return f"{self.name} ({self.rol})"
     
-class User(DateTime):
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=150)
-    active = models.BooleanField()
-    fk_rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
-    fk_category = models.ForeignKey(UserCategory, on_delete=models.CASCADE)
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
 class StudentParent(DateTime):
-    parent_id = models.AutoField(primary_key=True)
     rut = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
@@ -34,7 +78,6 @@ class StudentParent(DateTime):
         return f"{self.name} {self.lastName}"
 
 class Student(DateTime):
-    student_id = models.AutoField(primary_key=True)
     rut = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=40)
     lastName = models.CharField(max_length=50)
@@ -45,6 +88,8 @@ class Student(DateTime):
     grade = models.CharField(max_length=40)
     etnia = models.CharField(max_length=40, null=True, blank=True)
     fk_parent = models.ForeignKey(StudentParent, on_delete=models.CASCADE)
+    fk_curso = models.ForeignKey(Curso, null=True, on_delete=models.CASCADE)
+    fk_nivel = models.ForeignKey(Nivel, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} {self.lastName}"
